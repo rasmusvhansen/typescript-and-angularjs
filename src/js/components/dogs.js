8f28c6e85d4072cf9465bb0964e47ba726a5cf0c
Rasmus Vestergaard Hansen <Rasmus.Vestergaard.Hansen@systematic.com>
2017-09-27 09:27:49 +0200

    Styling and cleanup

--- src/js/components/dogs.js
+++ src/js/components/dogs.js
@@ -21,10 +21,11 @@
       controller: DogsController,
       template:
         '<div>' +
-        '  <div><h1>{{$ctrl.selectedDog.breed}}</h1></div>' +
+        '  <div><h2>{{$ctrl.selectedDog.breed}}</h2></div>' +
         '  <dog-image breed="$ctrl.selectedDog.breed"></dog-image>' +
         '  <ul>' +
-        '    <li ng-repeat="dog in $ctrl.dogs"><a href ng-click="$ctrl.selectDog(dog)">{{dog.breed}}</a>' +
+        '    <li ng-repeat="dog in $ctrl.dogs">' +
+        '      <a href ng-click="$ctrl.selectDog(dog)">{{dog.breed}}</a>' +
         '      <ul ng-if="dog.subBreeds.length">' +
         '        <li ng-repeat="subBreed in dog.subBreeds">{{subBreed}}</li>' +
         '      </ul>' +
