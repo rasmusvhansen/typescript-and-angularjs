55cbc44c972aede53d42217dacdff4be13aaef53
Rasmus Vestergaard Hansen <Rasmus.Vestergaard.Hansen@systematic.com>
2017-09-27 09:01:30 +0200

    Proper indentation of template

--- src/js/components/dogs.js
+++ src/js/components/dogs.js
@@ -21,15 +21,15 @@
       controller: DogsController,
       template:
         '<div>' +
-        '<div><h1>{{$ctrl.selectedDog.breed}}</h1></div>' +
-        '<dog-image breed="$ctrl.selectedDog.breed"></dog-image>' +
-        '<ul>' +
-        '<li ng-repeat="dog in $ctrl.dogs"><a href ng-click="$ctrl.selectDog(dog)">{{dog.breed}}</a>' +
-        '<ul ng-if="dog.subBreeds.length">' +
-        '<li ng-repeat="subBreed in dog.subBreeds">{{subBreed}}</li>' +
-        '</ul>' +
-        '</li> ' +
-        '</ul>  ' +
+        '  <div><h1>{{$ctrl.selectedDog.breed}}</h1></div>' +
+        '  <dog-image breed="$ctrl.selectedDog.breed"></dog-image>' +
+        '  <ul>' +
+        '    <li ng-repeat="dog in $ctrl.dogs"><a href ng-click="$ctrl.selectDog(dog)">{{dog.breed}}</a>' +
+        '      <ul ng-if="dog.subBreeds.length">' +
+        '        <li ng-repeat="subBreed in dog.subBreeds">{{subBreed}}</li>' +
+        '      </ul>' +
+        '    </li> ' +
+        '  </ul>  ' +
         '</div>'
     });
 })();
